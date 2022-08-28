5427aada21186107671d9d0869e4bb1eec1aa6f4
Jonathan Whitmore <jbwhit@gmail.com>
2022-08-27 17:56:27 -0700

    Update

--- throwaway6/_modidx.py
+++ throwaway6/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://jbwhit.github.io',
                 'git_url': 'https://github.com/jbwhit/throwaway6',
                 'lib_path': 'throwaway6'},
-  'syms': {'throwaway6.core': {'throwaway6.core.foo': ('core.html#foo', 'throwaway6/core.py')}}}
+  'syms': { 'throwaway6.core': { 'throwaway6.core.cache_data': ('core.html#cache_data', 'throwaway6/core.py'),
+                                 'throwaway6.core.sample_from_spreadsheet': ('core.html#sample_from_spreadsheet', 'throwaway6/core.py')}}}
